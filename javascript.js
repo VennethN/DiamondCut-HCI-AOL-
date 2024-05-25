3012286679a8317f4fe91ce5c3d230eab7c9dca4
Valent Nathanael <77603243+VennethN@users.noreply.github.com>
2024-05-25 15:02:45 +0700

    car pages, car images, js data structure

--- javascript.js
+++ javascript.js
@@ -1,7 +1,63 @@
-window.addEventListener('scroll', function() {
-    const parallax = document.querySelectorAll('.parallax');
-    for (let i = 0; i < parallax.length; i++) {
-        let scroll = window.pageYOffset;
-        parallax[i].style.backgroundPositionY = scroll * 0.5 + 'px';
+var carsInfo = {
+    "zephyrx8": {
+        title: "Zephyr X8",
+        description: "Move like a thunderbolt in the storm",
+        imgSrc:"images/car1.webp",
+        priceText: "Starting at $20,000",
+    },
+    "veritas": {
+        title: "Veritas",
+        description: "Ride the sunset with this old but gold relic",
+        imgSrc:"images/car2.webp",
+        priceText: "Starting at $25,000",
+    },
+    "auroragts": {
+        title: "Aurora GTS",
+        description: "Efficiency at its peak",
+        imgSrc:"images/car3.webp",
+        priceText: "Starting at $35,000",
+    },
+    "nexura": {
+        title: "Nexura",
+        description: "Futuristic design with a touch of elegance",
+        imgSrc:"images/car4.webp",
+        priceText: "Starting at $70,000",
+    },
+    "zenith": {
+        title: "Zenith",
+        description: "The winter soldier",
+        imgSrc:"images/car5.webp",
+        priceText: "Starting at $50,000",
+    },
+    "cobaltz1": {
+        title: "Cobalt-Z1",
+        description: "The blue beast",
+        imgSrc:"images/car6.webp",
+        priceText: "Starting at $150,000",
+    },
+    "crimsonvortex": {
+        title: "Crimson Vortex",
+        description: "The red devil",
+        imgSrc:"images/car7.webp",
+        priceText: "Starting at $200,000",
     }
-});
+    // Add more sections as needed
+};
+
+var topCars = [
+    "cobaltz1",
+    "veritas",
+    "crimsonvortex"
+];
+
+var usedCars = [
+
+]
+var newCars = [
+    "cobaltz1",
+    "zephyrx8",
+    "auroragts",
+    "nexura",
+    "zenith",
+    "crimsonvortex"
+]
