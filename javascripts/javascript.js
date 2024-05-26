784ddba68196b4e17bb44c79d7ddcd9d0ac7f90d
Valent Nathanael <77603243+VennethN@users.noreply.github.com>
2024-05-26 17:32:14 +0700

    file refactor, progress on showroom, refactor parallax

---
+++ javascripts/javascript.js
@@ -0,0 +1,138 @@
+var carsInfo = {
+    "zephyrx8": {
+        title: "Zephyr X8",
+        description: "Move like a thunderbolt in the storm",
+        image:"../images/car1.webp",
+        priceText: "Starting at $40,000",
+    },
+    "veritas": {
+        title: "Veritas",
+        description: "Ride the sunset with this old but gold relic",
+        image:"../images/car2.webp",
+        priceText: "Starting at $25,000",
+    },
+    "auroragts": {
+        title: "Aurora GTS",
+        description: "Efficiency at its peak",
+        image:"../images/car3.webp",
+        priceText: "Starting at $35,000",
+    },
+    "nexura": {
+        title: "Nexura",
+        description: "Futuristic design with a touch of elegance",
+        image:"../images/car4.webp",
+        priceText: "Starting at $70,000",
+    },
+    "zenith": {
+        title: "Zenith X8",
+        description: "The winter soldier",
+        image:"../images/car5.webp",
+        priceText: "Starting at $50,000",
+    },
+    "cobaltz1": {
+        title: "Cobalt-Z1",
+        description: "The blue beast",
+        image:"../images/car6.webp",
+        priceText: "Starting at $150,000",
+    },
+    "crimsonvortex": {
+        title: "Crimson Vortex",
+        description: "The red devil",
+        image:"../images/car7.webp",
+        priceText: "Starting at $200,000",
+    },
+    "modelA": {
+        title: "Model A",
+        description: "The classic",
+        image:"../images/car1.webp",
+        priceText: "Starting at $20,000",
+    },
+    "modelB": {
+        title: "Model B",
+        description: "Another classic, a step up",
+        image:"../images/car1.webp",
+        priceText: "Starting at $25,000",
+    },
+    "porsche911": {
+        title: "Porsche 911",
+        description: "The classic porsche 911",
+        image:"../images/car-porsche911.webp",
+        priceText: "Starting at $25,000",
+    },
+    "teslacybertruck": {
+        title: "Tesla Cybertruck",
+        description: "The futuristic truck",
+        image:"../images/car-teslacybertruck.webp",
+        priceText: "Starting at $25,000",
+    },
+    "toyotacorolla": {
+        title: "Toyota Corolla",
+        description: "The reliable car",
+        image:"../images/car-toyotacorolla.webp",
+        priceText: "Starting at $11,800",
+    },
+    "hondacivic": {
+        title: "Honda Civic",
+        description: "The Power of Dreams.",
+        image:"../images/car-hondacivic.webp",
+        priceText: "Starting at $23,950",
+    },
+    "ferrariroma": {
+        title: "Ferrari Roma",
+        description: "The classic Ferrari",
+        image:"../images/car-ferrariroma.webp",
+        priceText: "Starting at $25,000",
+    },
+    "astonmartinvalhalla":{
+        title: "Aston Martin Valhalla",
+        description: "Experience the Pinnacle of Performance.",
+        image:"../images/car-astonmartinvalhalla.webp",
+        priceText: "Starting at $800,000",
+    },
+    "mercedesbenzamgs63eperformance":{
+        title: "Mercedes Benz AMG S63 E Performance",
+        description: "The future of luxury",
+        image:"../images/car-mercedesbenzamgs63eperformance.webp",
+        priceText: "Starting at $190,000",
+    },
+    "ferraripurosangue":{
+        title: "Ferrari Purosangue",
+        description: "The first SUV from Ferrari",
+        image:"../images/car-ferraripurosangue.webp",
+        priceText: "Starting at $400,000",
+    },
+    "teslamodely":{
+        title: "Tesla Model Y",
+        description: "The future of SUVs",
+        image:"../images/car-teslamodely.webp",
+        priceText: "Starting at $40,000",
+    }
+    // Add more sections as needed
+};
+
+var topCars = [
+    "porsche911",
+    "toyotacorolla",
+    "hondacivic"
+];
+
+var oldCars = [
+    "ferrariroma",
+    "porsche911",
+    "hondacivic",
+    "toyotacorolla",
+    "modelB",
+]
+var newCars = [
+    "astonmartinvalhalla",
+    "mercedesbenzamgs63eperformance",
+    "ferraripurosangue",
+    "teslamodely",
+]
+
+var workingBrands ={
+    "brandTest":{
+        img : "images/car1.webp",
+        alt : "car1",
+    }
+};
